cd962875a8731b57c7e164cf99faf089f2155f17
nortonlyr <nortonlyr@gmail.com>
2020-08-16 00:20:49 -0400

    Updated Java ex

---
+++ Intro_to_Java_Programming/src/E1_01.java
@@ -0,0 +1,8 @@
+
+public class E1_01 {
+	public static void main(String[] args) {
+	System.out.println("Welcome to Java");
+	System.out.println("Welcome to CS");
+	System.out.println("Programming is fun");
+	}
+}
