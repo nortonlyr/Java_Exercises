c20bfc8dd3487e6f1846c340d4e6c225d7ea331c
nortonlyr <nortonlyr@gmail.com>
2020-08-18 01:04:21 -0400

    Create E2_08.java

---
+++ Intro_to_Java_Programming/src/E2/E2_08.java
@@ -0,0 +1,11 @@
+/*
+  Listing 2.7, ShowCurrentTime.java, gives a program that displays the current
+  time in GMT. Revise the program so that it prompts the user to enter the
+  time zone offset to GMT and displays the time in the specified time zone.
+*/
+
+import java.util.Scanner;
+
+public class E2_08 {
+
+}
