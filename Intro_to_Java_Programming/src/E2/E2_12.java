39680fb921f6780233c587ecb3ceb10437e310a1
nortonlyr <nortonlyr@gmail.com>
2020-08-21 16:49:11 -0400

    Update E2_12.java

--- Intro_to_Java_Programming/src/E2/E2_12.java
+++ Intro_to_Java_Programming/src/E2/E2_12.java
@@ -8,6 +8,21 @@
   minimum runway length.
 */
 
-public class E2_12 {
+import java.util.Scanner;
 
+public class E2_12 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter speed and acceleration: ");
+		double speed = input.nextDouble();
+		double acceleration = input.nextDouble();
+
+		double runwayLength = calculateRunwayLength(speed, acceleration);
+
+		System.out.println("The minimum runway length for this airplane is " + runwayLength);
+	}
+
+	private static double calculateRunwayLength(double s, double a) {
+		return (s * s) / (2 * a);
+	}
 }
