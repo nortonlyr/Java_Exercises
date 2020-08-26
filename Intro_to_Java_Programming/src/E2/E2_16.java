db730795816b550fc7b705021499532088fa3385
nortonlyr <nortonlyr@gmail.com>
2020-08-26 00:59:11 -0400

    Update E2_16.java

--- Intro_to_Java_Programming/src/E2/E2_16.java
+++ Intro_to_Java_Programming/src/E2/E2_16.java
@@ -7,5 +7,17 @@
 import java.util.Scanner;
 
 public class E2_16 {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the side: ");
+		double side = input.nextDouble();
+		
+		double area = areaOfHexagon(side);
+		
+		System.out.println("The area of the hexagon is  " + area);
+	}
+	
+	private static double areaOfHexagon(double s) {
+		return ((3.0 * Math.sqrt(3)) / 2.0) * (s *s);
+	}
 }
