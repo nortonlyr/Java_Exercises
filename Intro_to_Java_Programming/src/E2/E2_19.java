fe923c2b19f2c65d6eb2454a6b4c11d8c97c696b
nortonlyr <nortonlyr@gmail.com>
2020-08-27 23:20:45 -0400

    Create E2_19.java

---
+++ Intro_to_Java_Programming/src/E2/E2_19.java
@@ -0,0 +1,16 @@
+/*
+  Write a program that prompts the user to enter three points
+  (x1, y1), (x2, y2), (x3, y3) of a triangle and displays its area. The formula
+  for computing the area of a triangle is
+  s = (side1 + side2 + side3) / 2
+  area = sqrt(s(s - side1)(s - side2)(s- side3))
+*/
+
+import java.util.Scanner;
+
+public class E2_19 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter three points for a triangle: ");
+	}
+}
