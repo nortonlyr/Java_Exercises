6f99fbdfc7709a02a00cec145b769f2557c2a184
nortonlyr <nortonlyr@gmail.com>
2020-08-28 23:32:00 -0400

    Create E2_20.java

---
+++ Intro_to_Java_Programming/src/E2/E2_20.java
@@ -0,0 +1,11 @@
+/*
+  If you know the balance and the annual percentage interest rate, you can
+  compute the interest on the next monthly payment using the following formula:
+  interest = balance x (annualInterestRate / 1200)
+  Write a program that reads the balance and the annual percentage interest
+  rate and displays the interest for the next month.
+*/
+
+public class E2_20 {
+
+}
