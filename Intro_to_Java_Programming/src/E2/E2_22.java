3f002e6249ed5826d94a59e2da284517647ab3bb
nortonlyr <nortonlyr@gmail.com>
2020-08-31 00:09:26 -0400

    Update E2_22.java

--- Intro_to_Java_Programming/src/E2/E2_22.java
+++ Intro_to_Java_Programming/src/E2/E2_22.java
@@ -11,5 +11,18 @@ public class E2_22 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter an amount in int, for example 1156 " + "(11 dollars and 56 cents): ");
+		int amount = input.nextInt();
+		
+		int remainingAmount = amount;
+		
+		int numberOfOneDollars = remainingAmount / 100;
+		remainingAmount = remainingAmount % 100;
+		int numberOfQuarters = remainingAmount / 25;
+		remainingAmount = remainingAmount % 25;
+		int numberOfDimes = remainingAmount / 10;
+		remainingAmount = remainingAmount % 10;
+		int numberOfNickels = remainingAmount / 5;
+		remainingAmount = remainingAmount % 5;
+		int numberofPennies = remainingAmount;
 	}
 }
