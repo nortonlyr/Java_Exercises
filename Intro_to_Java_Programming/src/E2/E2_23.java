a238682e4ca9d6b9cd64d0d8500b811fd468f9f9
nortonlyr <nortonlyr@gmail.com>
2020-09-02 00:46:02 -0400

    Create E2_23.java

---
+++ Intro_to_Java_Programming/src/E2/E2_23.java
@@ -0,0 +1,9 @@
+/*
+  Write a program that prompts the user to enter the distance to drive, the
+  fuel efficiency of the car in miles per gallon, and the price per gallon,
+  and displays the cost of the trip.
+*/
+
+public class E2_23 {
+
+}
