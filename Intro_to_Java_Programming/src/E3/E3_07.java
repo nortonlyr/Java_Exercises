2a5b771d4a6560616cbb51e39fb713dfb9cce11c
nortonlyr <nortonlyr@gmail.com>
2020-09-10 00:55:21 -0400

    Update E3_07.java

--- Intro_to_Java_Programming/src/E3/E3_07.java
+++ Intro_to_Java_Programming/src/E3/E3_07.java
@@ -11,15 +11,55 @@ public class E3_07 {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter an amount in double, for example 11.56: ");
 		double amount = input.nextDouble();
-		
-		int remainingAmount = (int)(amount * 100);
+
+		int remainingAmount = (int) (amount * 100);
 		int[] denominations = new int[5];
-		
+
 		denominations[0] = remainingAmount / 100;
 		remainingAmount = remainingAmount % 100;
 		denominations[1] = remainingAmount / 25;
 		remainingAmount = remainingAmount % 25;
 		denominations[2] = remainingAmount / 10;
 		remainingAmount = remainingAmount % 10;
+		denominations[3] = remainingAmount / 5;
+		remainingAmount = remainingAmount % 5;
+		denominations[4] = remainingAmount;
+
+		System.out.println("Your amount " + amount + " consists of ");
+		for (int i = 0; i < denominations.length; i++) {
+			if (denominations[i] > 0) {
+				int value = denominations[i];
+				String denominationName = denominationName(i, value);
+				System.out.println("    " + value + " " + denominationName);
+			}
+		}
+	}
+
+	private static String denominationName(int denom, int value) {
+		StringBuilder output = new StringBuilder();
+
+		if (value == 0) {
+			output.append("");
+		} else {
+			switch (denom) {
+			case 0:
+				output.append(value > 1 ? "dollars" : "dollar");
+				break;
+			case 1:
+				output.append(value > 1 ? "quarters" : "quarter");
+				break;
+			case 2:
+				output.append(value > 1 ? "dimes" : "dime");
+				break;
+			case 3:
+				output.append(value > 1 ? "nickels" : "nickel");
+				break;
+			case 4:
+				output.append(value > 1 ? "pennies" : "penny");
+				break;
+
+			}
+		}
+		return output.toString();
 	}
 }
