765c062533c8035a45d97e6e21f5c204c5ff02a5
nortonlyr <nortonlyr@gmail.com>
2020-09-11 00:03:27 -0400

    Create E3_09.java

---
+++ Intro_to_Java_Programming/src/E3/E3_09.java
@@ -0,0 +1,14 @@
+/*
+  An ISBN-10 (International Standard Book Number) consists of 10 digits. The
+  last digit is a checksum which is calculated from the other nine digits using
+  the following formula:
+                    (formula)
+  If the checksum is 10, the last digit is denoted as X according to the
+  ISBN-10 convention. Write a program that prompts the user to enter the first
+  9 digits and displays the 10-digit ISBN (including leading zeros). Your
+  program should read the input as an integer.
+*/
+
+public class E3_09 {
+
+}
