229149b7e5ffa1bf83acc8574d1d0240d223624e
nortonlyr <nortonlyr@gmail.com>
2020-09-12 00:00:55 -0400

    Update E3_09.java

--- Intro_to_Java_Programming/src/E3/E3_09.java
+++ Intro_to_Java_Programming/src/E3/E3_09.java
@@ -9,6 +9,40 @@
   program should read the input as an integer.
 */
 
-public class E3_09 {
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class E3_09 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the first 9 digits of an ISBN as integer: ");
+		String digitString = input.nextLine();
+		
+		ArrayList<Integer> digits = new ArrayList<>();
+		for (int i = 0; i < digitString.length(); i++) {
+			int digit = Integer.parseInt(digitString.charAt(i) + "");
+			digits.add(digit);
+		}
+		
+		String isbn = digitString + isbnChecksum(digits);
+		
+		System.out.println("The ISBN-10 number is " + isbn);
+	}
+	
+	private static String isbnChecksum(ArrayList<Integer> digit) {
+		int checksum = 0;
+		for (int i = 0; i < digit.size(); i++) {
+			checksum += digit.get(i) * (i + 1);
+		}
+		checksum %= 11;
+		
+		StringBuilder output = new StringBuilder();
+		if (checksum == 10) {
+			output.append("X");
+		} else {
+			output.append(checksum);
+		}
+		
+		return output.toString();
+	}
 }
