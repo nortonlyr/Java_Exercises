20e1ec572f198f3650f7042fdd89f0d99db2d1da
nortonlyr <nortonlyr@gmail.com>
2020-10-29 17:19:15 -0400

    Create Dog.java

---
+++ java_small_applications/Dog.java
@@ -0,0 +1,12 @@
+
+public class Dog {
+	protected void bark() {
+		System.out.println("ÍôÍô£¬ß^í");
+	}
+}
+
+class Teddy extends Dog {
+	void bark() {
+		System.out.println("ÍôÍô£¬íß@ß…¸úÖøÎÒ");
+	}
+}
