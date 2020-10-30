cadfc1e1fba1395efeca0fed86fa464271cf96c4
nortonlyr <nortonlyr@gmail.com>
2020-10-29 20:24:19 -0400

    Create java_grammar_var_001.java

---
+++ java_small_applications/java_grammar_var_001.java
@@ -0,0 +1,12 @@
+
+public class java_grammar_var_001 {
+	public static void main(String[] args) {
+		String s1 = "hello,world";
+		String[] arr = s1.split(",");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+				
+	}
+
+	}
+}
