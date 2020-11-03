1c75aa73841cf445fdfeae63004194434e0b4173
nortonlyr <nortonlyr@gmail.com>
2020-11-03 18:31:12 -0500

    Create E3_12.java

---
+++ Intro_to_Java_Programming/src/E3/E3_12.java
@@ -0,0 +1,26 @@
+
+ /*
+(Palindrome number) Write a program that prompts the user to enter a three-digit
+integer and determines whether it is a palindrome number. A number is palindrome
+if it reads the same from right to left and from left to right.
+*/
+
+import java.util.Scanner;
+
+public class E3_12 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user to enter a three-digit integer
+		System.out.println("Enter a three-digit integer: ");
+		int number = input.nextInt();
+		
+		// Test for palidrome
+		int digit1 = (int)(number / 100);
+		int remaining = number % 100;
+		int digit3 = (int)(remaining % 10);
+		
+		// Display result
+		System.out.println( number + ((digit1 == digit3) ? " is a " : " is not a ") + "palindrome");
+	}
+}
