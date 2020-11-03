6677a9857ab7cb14461576cb3b92891f73934ad4
nortonlyr <nortonlyr@gmail.com>
2020-11-03 16:03:02 -0500

    Update E3_11.java

--- Intro_to_Java_Programming/src/E3/E3_11.java
+++ Intro_to_Java_Programming/src/E3/E3_11.java
@@ -23,10 +23,18 @@ public class E3_11 {
 		// Display the number of days in the month
 		switch (month)
 		{
-			case 1: System.out.println("January " + year + "had 31 days"); break;
+			case 1: System.out.println("January " + year + " had 31 days"); break;
 			case 2: System.out.println((leapYear) ? "29 days": " 28 days"); break;
-			case 1: System.out.println("March " + year + "had 31 days"); break;
+			case 3: System.out.println("March " + year + " had 31 days"); break;
+			case 4: System.out.println("April " + year + " had 30 days"); break;
+			case 5: System.out.println("May " + year + " had 31 days"); break;
+			case 6: System.out.println("June " + year + " had 30 days"); break;
+			case 7: System.out.println("July " + year + " had 31 days"); break;
+			case 8: System.out.println("August " + year + " had 31 days"); break;
+			case 9: System.out.println("September " + year + " had 30 days"); break;
+			case 10: System.out.println("October " + year + " had 31 days"); break;
+			case 11: System.out.println("Noevember " + year + " had 30 days"); break;
+			case 12: System.out.println("December " + year + " h2ad 31 days"); break;
 		}
 	}
-	
 }
