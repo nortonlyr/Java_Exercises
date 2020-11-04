1e8bad5f0bc490d8f04af5bdbde95d244d25f245
nortonlyr <nortonlyr@gmail.com>
2020-11-04 00:42:36 -0500

    Update E3_13.java

--- Intro_to_Java_Programming/src/E3/E3_13.java
+++ Intro_to_Java_Programming/src/E3/E3_13.java
@@ -17,6 +17,29 @@ public class E3_13 {
 		
 		// Prompt the user to enter taxable income
 		System.out.print("Enter the taxable income");
+		double income = input.nextDouble();
+		
+		// Compute tax
+		double tax =  0;
+		switch (status)
+		{
+			case 0 : // Compute tax for single filers
+				tax += (income <= 8350) ? income * 0.10 : 8350 * 0.10;
+				if (income > 8350)
+					tax += (income <= 33950) ? (income - 8350) * 0.15 :
+					25600 * 0.15;
+				if (income > 33950)
+					tax += (income <= 82250) ? (income - 33950) * 0.25 :
+					48300 * 0.25;
+				if (income > 82250)
+					tax += (income <= 171550) ? (income - 82250) * 0.28 :
+					89300 * 0.28;
+				if (income > 171550)
+					tax += (income <= 372950) ? (income - 171550) * 0.33 :
+					201400 * 0.33;
+				if (income > 372950)
+					tax += (income - 372950) * 0.35;
+		}
 	}
 	
 }
