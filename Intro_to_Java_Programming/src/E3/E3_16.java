d619b63cb7f6298671a8da1d78f51cc9e858974a
nortonlyr <nortonlyr@gmail.com>
2020-11-05 00:11:08 -0500

    Update E3_16.java

--- Intro_to_Java_Programming/src/E3/E3_16.java
+++ Intro_to_Java_Programming/src/E3/E3_16.java
@@ -5,5 +5,13 @@ The rectangle is centered at (0, 0) with width 100 and height 200.
 
 
 public class E3_16 {
-
+	public static void main(String[] args) {
+		// Generate random width and height 
+		int width = (int)((Math.random()*(50+50))-50);
+		int height = (int)((Math.random()*(100+100))-100);
+		
+		// Display coordinate
+		System.out.println("Random coordinate in rectangle centered at (0,0)");
+		System.out.println("with width 100 and height 200: (" + width + ", " + height + ")");
+	}
 }
