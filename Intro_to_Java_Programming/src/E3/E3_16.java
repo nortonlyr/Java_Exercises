8af4dac6c7d42cc4dd88d0f715674d546064c8b0
nortonlyr <nortonlyr@gmail.com>
2020-11-04 20:45:22 -0500

    Create E3_16.java

---
+++ Intro_to_Java_Programming/src/E3/E3_16.java
@@ -0,0 +1,9 @@
+/*
+(Random point) Write a program that displays a random coordinate in a rectangle.
+The rectangle is centered at (0, 0) with width 100 and height 200.
+*/
+
+
+public class E3_16 {
+
+}
