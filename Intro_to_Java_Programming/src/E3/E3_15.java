59ea4909409a5412107749e95bcfecf6c05f1b8a
nortonlyr <nortonlyr@gmail.com>
2020-11-04 19:58:28 -0500

    Create E3_15.java

---
+++ Intro_to_Java_Programming/src/E3/E3_15.java
@@ -0,0 +1,16 @@
+/*
+(Game: lottery) Revise Listing 3.8, Lottery.java, to generate a lottery of a threedigit
+number. The program prompts the user to enter a three-digit number and
+determines whether the user wins according to the following rules:
+1. If the user input matches the lottery number in the exact order, the award is
+$10,000.
+2. If all digits in the user input match all digits in the lottery number, the award is
+$3,000.
+3. If one digit in the user input matches a digit in the lottery number, the award is
+$1,000.
+*/
+import java.util.Scanner;
+
+public class E3_15 {
+
+}
