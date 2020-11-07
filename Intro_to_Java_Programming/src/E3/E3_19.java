7037f3b9569418a045db198200faa49d0ad10e64
nortonlyr <nortonlyr@gmail.com>
2020-11-07 11:06:49 -0500

    Update E3_19.java

--- Intro_to_Java_Programming/src/E3/E3_19.java
+++ Intro_to_Java_Programming/src/E3/E3_19.java
@@ -8,5 +8,16 @@ greater than the remaining edge.
 import java.util.Scanner;
 
 public class E3_19 {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user to enter three edges for a triangle
+		System.out.println("Enter three edges for a triangle:");
+		System.out.print(" Edge 1 points x, y: ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		System.out.print(" Edge 2 points x, y: ");
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+	}
 }
