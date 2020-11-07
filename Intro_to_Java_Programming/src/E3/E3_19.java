528281661ad9211282750028c14a0db648ac1c6d
nortonlyr <nortonlyr@gmail.com>
2020-11-07 12:26:00 -0500

    Update E3_19.java

--- Intro_to_Java_Programming/src/E3/E3_19.java
+++ Intro_to_Java_Programming/src/E3/E3_19.java
@@ -27,5 +27,19 @@ public class E3_19 {
 		boolean valid = (x1 + y1 > x3 + y3 && x2 + y2 > x3 + y3) || 
 				(x1 + y1 > x2 + y2 && x3 + y3 > x2 + y2) ||
 				x3 + y3 > x1 + y1 && x2 + y2 > x1 + y1);
+				
+		if (!valid)
+		{
+			System.out.println("Input is invalid.");
+			System.exit(1));
+		}
+		
+		// Compute the sides of the triangle 
+		double side1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+		double side2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
+		double side3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
+		
+		// Display the perimeter of the triangle
+		System.out.println("perimeter of triangle is " + (side1 + side2 + side3));
 	}
 }
