9febbcd36e0c610c0694dffcc2ae22ae8e933a52
nortonlyr <nortonlyr@gmail.com>
2020-11-07 20:18:21 -0500

    Create E3_20.java

---
+++ Intro_to_Java_Programming/src/E3/E3_20.java
@@ -0,0 +1,19 @@
+/*
+(Science: wind-chill temperature) Programming Exercise 2.17 gives a formula
+to compute the wind-chill temperature.The formula is valid for temperatures in
+the range between -58F and 41F and wind speed greater than or equal to 2.
+Write a program that prompts the user to enter a temperature and a wind speed.
+The program displays the wind-chill temperature if the input is valid; otherwise,
+it displays a message indicating whether the temperature and/or wind speed is
+invalid.
+
+*/
+
+
+import java.util.Scaner;
+
+public class E3_20 {
+	public static void main(String[] args) {
+		
+	}
+}
