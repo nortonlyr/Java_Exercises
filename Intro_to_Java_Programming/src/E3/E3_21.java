ab0988d0cad6046be7d9c0fbee80f8fe3f0e272a
nortonlyr <nortonlyr@gmail.com>
2020-11-09 00:07:19 -0500

    Create E3_21.java

---
+++ Intro_to_Java_Programming/src/E3/E3_21.java
@@ -0,0 +1,10 @@
+/*
+(Science: day of the week) Zeller¡¯s congruence is an algorithm developed by
+Christian Zeller to calculate the day of the week.
+*/
+
+import java.util.Scanner;
+
+public class E3_21 {
+
+}
