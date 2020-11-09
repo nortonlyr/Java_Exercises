f4d1e95034e4b0888351f9f3c16c8f57573a62d1
nortonlyr <nortonlyr@gmail.com>
2020-11-08 19:56:29 -0500

    Update E3_20.java

--- Intro_to_Java_Programming/src/E3/E3_20.java
+++ Intro_to_Java_Programming/src/E3/E3_20.java
@@ -10,7 +10,7 @@ invalid.
 */
 
 
-import java.util.Scaner;
+import java.util.Scanner;
 
 public class E3_20 {
 	public static void main(String[] args) {
@@ -18,5 +18,27 @@ public class E3_20 {
 		
 		// Prompt the user to enter a temperature and a wind speed
 		System.out.print("Enter the temperature in Fahrenheit " + "between -58F and 41F: ");
+		double temperature = input.nextDouble(); 
+		System.out.print("Enter the wind speed (>= 2) in miles per hour: ");
+		double speed = input.nextDouble();
+		
+		if (temperature <= -58 || temperature >= 41 || speed < 2)
+		{
+			System.out.print("The ");
+			if (temperature <= -58 || temperature >= 41)
+				System.out.print("temperature ");
+			if ((temperature <= -58 || temperature >= 41) && speed < 2)
+				System.out.print("and ");
+			if (speed < 2)
+				System.out.print("wind speed ");
+			System.out.print("is invalid");
+			System.exit(1);
+		}
+		
+		// Compute the wind chill index
+		double windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(speed, 0.16) + 0.4275 * Math.pow(speed, 0.16);
+		
+		// Display result
+		System.out.println("The wind chill index is " + windChill);
 	}
 }
