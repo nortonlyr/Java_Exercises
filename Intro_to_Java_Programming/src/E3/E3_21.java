62541a5b5c542f10e5cd4f748bb685c02443e3e4
nortonlyr <nortonlyr@gmail.com>
2020-11-09 20:36:33 -0500

    Update E3_21.java

--- Intro_to_Java_Programming/src/E3/E3_21.java
+++ Intro_to_Java_Programming/src/E3/E3_21.java
@@ -13,5 +13,26 @@ public class E3_21 {
 		System.out.print("Enter year: (e.g., 2012): ");
 		int year = input.nextInt();
 		System.out.print("Enter month: 1-12:");
+		int month = input.nextInt();
+		System.out.print("Enter the day of the month: 1-31: ");
+		int dayOfMonth = input.nextInt();
+		
+		// Convert January and February to months 13 and 14 of the previous year
+		if (month == 1 || month == 2)
+		{
+			month = (month == 1) ? 13 : 14;
+			year --;
+		}
+		
+		// Calculate day of the week
+		int dayOfWeek = (dayOfMonth + (26 * (month + 1)) / 10 + (year % 100) 
+				+ (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
+		
+		// Display result
+		System.out.print("Day of the week is ");
+		switch(dayOfWeek)
+		{
+		case 0: System.out.println("Saturday"); break;
+		}
 	}
 }
