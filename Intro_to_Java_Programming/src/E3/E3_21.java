e83229a3f64b27704fc6791d8286c0d5c27a0277
nortonlyr <nortonlyr@gmail.com>
2020-11-09 20:11:05 -0500

    Update E3_21.java

--- Intro_to_Java_Programming/src/E3/E3_21.java
+++ Intro_to_Java_Programming/src/E3/E3_21.java
@@ -6,5 +6,12 @@ Christian Zeller to calculate the day of the week.
 import java.util.Scanner;
 
 public class E3_21 {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Prompy the user to enter a year, month, and day of the month.
+		System.out.print("Enter year: (e.g., 2012): ");
+		int year = input.nextInt();
+		System.out.print("Enter month: 1-12:");
+	}
 }
