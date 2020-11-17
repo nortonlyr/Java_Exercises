389f64e2350acd879cf575007767385e66219f2d
nortonlyr <nortonlyr@gmail.com>
2020-11-17 17:09:17 -0500

    Update E3_23.java

--- Intro_to_Java_Programming/src/E3/E3_23.java
+++ Intro_to_Java_Programming/src/E3/E3_23.java
@@ -19,5 +19,13 @@ public class E3_23 {
 		System.out.print("Enter a point with two coordinates: ");
 		double x = input.nextDouble();
 		double y = input.nextDouble();
+		
+		// check whether the point is within the rectangle 
+		// centered at (0,0) with width 10 and height 5
+		boolean withinRectangle = (Math.pow(Math.pow(x, 2), 0.5) <= 10 / 2) 
+				|| (Math.pow(Math.pow(y,2), 0.5) <= 5.0 / 2);
+				
+		// Display results
+		System.out.println("Point (" + x + ", " + y + ") is " + ((withinRectangle) ? "in " : "not in ") + "the rectangle");
 	}
 }
