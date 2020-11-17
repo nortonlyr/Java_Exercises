d2d7f512b32e1a2204fe7caf862ef0321bfbda95
nortonlyr <nortonlyr@gmail.com>
2020-11-16 23:13:48 -0500

    Create E3_22.java

---
+++ Intro_to_Java_Programming/src/E3/E3_22.java
@@ -0,0 +1,14 @@
+/*
+(Geometry: point in a circle?) Write a program that prompts the user to enter a
+point (x, y) and checks whether the point is within the circle centered at (0, 0)
+with radius 10. For example, (4, 5) is inside the circle and (9, 9) is outside the
+circle, as shown in Figure 3.7a.
+*/
+
+import java.util.Scanner;
+
+public class E3_22 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	}
+}
