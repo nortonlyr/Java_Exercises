9ede7d68b7c3ef9bdaddc651492275b1bcbd4159
nortonlyr <nortonlyr@gmail.com>
2020-11-17 23:13:16 -0500

    ATM Virtual Machine Exercise

---
+++ ATM_EX01/src/Test1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in)
+				
+		StringcarNum1 = "1001";
+		String password1 = "123456";
+		double balance1 = 10000;
+		
+		StringcarNum2 = "1001";
+		String password2 = "123456";
+		double balance2 = 20000;
+		
+		StringcarNum3 = "1001";
+		String password3 = "123456";
+		double balance3 = 30000;
+	}
+}
