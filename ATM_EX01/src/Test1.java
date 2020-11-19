6cdf3d0e05a733693007c1050197e821bd2d465b
nortonlyr <nortonlyr@gmail.com>
2020-11-18 22:00:53 -0500

    Update Test1.java

--- ATM_EX01/src/Test1.java
+++ ATM_EX01/src/Test1.java
@@ -6,15 +6,15 @@ public class Test1 {
 		
 		Scanner in = new Scanner(System.in)
 				
-		StringcardNum1 = "1001";
+		String cardNum1 = "1001";
 		String password1 = "123456";
 		double balance1 = 10000;
 		
-		StringcardNum2 = "1001";
+		String cardNum2 = "1001";
 		String password2 = "123456";
 		double balance2 = 20000;
 		
-		StringcardNum3 = "1001";
+		String cardNum3 = "1001";
 		String password3 = "123456";
 		double balance3 = 30000;
 		
@@ -25,9 +25,9 @@ public class Test1 {
 			System.out.println("请输入密码: ");
 			String password=in.next();
 			
-			if (carNum.equal2(cardNum1) && password.equals(password1)){
+			if (carNum.equals(cardNum1) && password.equals(password1)){
 				System.out.println("輸入正確！用戶: 卡1");
-			}elseif(carNum.equal2(cardNum2) && password.equals(password2){
+			}else if(carNum.equals(cardNum2) && password.equals(password2){
 				System.out.println("輸入正確！用戶: 卡2");
 			}
 		}
