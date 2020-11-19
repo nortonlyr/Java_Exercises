5cc09e24b445ce06a2166fcc95b424fac7b6864c
nortonlyr <nortonlyr@gmail.com>
2020-11-18 22:03:03 -0500

    Update Test1.java

--- ATM_EX01/src/Test1.java
+++ ATM_EX01/src/Test1.java
@@ -27,8 +27,12 @@ public class Test1 {
 			
 			if (carNum.equals(cardNum1) && password.equals(password1)){
 				System.out.println("輸入正確！用戶: 卡1");
-			}else if(carNum.equals(cardNum2) && password.equals(password2){
+			}else if(carNum.equals(cardNum2) && password.equals(password2)){
 				System.out.println("輸入正確！用戶: 卡2");
+			}else if(carNum.equals(cardNum3) && password.equals(password3)){
+				System.out.println("輸入正確！用戶: 卡3");
+			}else {
+				System.out.println("卡號密碼輸入錯誤，請重新輸入! ");
 			}
 		}
 	}
