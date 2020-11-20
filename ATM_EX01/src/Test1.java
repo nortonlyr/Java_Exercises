845fe45f24dfcf6251cd3a926a5920766b24f795
nortonlyr <nortonlyr@gmail.com>
2020-11-20 00:09:42 -0500

    Update Test1.java

--- ATM_EX01/src/Test1.java
+++ ATM_EX01/src/Test1.java
@@ -30,16 +30,20 @@ public class Test1 {
 			if (carNum.equals(cardNum1) && password.equals(password1)){
 				System.out.println("輸入正確！用戶: 卡1");
 				balance = balance1;
-				break;
+				//break;
 			}else if(carNum.equals(cardNum2) && password.equals(password2)){
 				System.out.println("輸入正確！用戶: 卡2");
 				balance=balance2;
-				break;
+				//break;
 			}else if(carNum.equals(cardNum3) && password.equals(password3)){
 				System.out.println("輸入正確！用戶: 卡3");
 				balance=balance3;
-				break;
+				//break;
 			}else {
+				count++;
+				if (count >= 3) {
+					System.out.println("您已經三次輸入錯誤，銀行卡已被鎖定! ");
+				}
 				System.out.println("卡號密碼輸入錯誤，請重新輸入! ");
 				continue;
 			}
