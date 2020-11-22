24148b7a0f324fc25e83093f60ea6da4deb6b601
nortonlyr <nortonlyr@gmail.com>
2020-11-21 22:34:36 -0500

    Update Shopping_system.java

--- Shopping_system/src/Shopping_system.java
+++ Shopping_system/src/Shopping_system.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Shopping_system {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+		System.out.println("===购物管理系统===");
+		System.out.println("======1.登录======");
+		System.out.println("======2.退出======");
+		System.out.println("请选择: ");
+	}
 	}
 }
