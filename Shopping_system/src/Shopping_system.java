1a3a36279d2ac973be579e236f05a28618d7d57a
nortonlyr <nortonlyr@gmail.com>
2020-11-21 22:55:56 -0500

    Update Shopping_system.java

--- Shopping_system/src/Shopping_system.java
+++ Shopping_system/src/Shopping_system.java
@@ -7,6 +7,21 @@ public class Shopping_system {
 		System.out.println("======1.登录======");
 		System.out.println("======2.退出======");
 		System.out.println("请选择: ");
+		int choice1 = in.nextInt();
+		if(choice1==1) {
+			int count=0;
+			while(1==1) {
+				System.out.println("请输入用户名: ");
+				String name = in.next();
+				System.out.println("请输入密码: ");
+				String pwd = in.next();
+				
+				if("aaa".equals(name)&& "123".equals(pwd)) {
+					while(1==1) {
+						System.out.println("==============主菜单============");
+					}
+				}
+			}
 	}
 	}
 }
