27b0b177bb516568a132596e5266fb18e624939f
nortonlyr <nortonlyr@gmail.com>
2020-11-23 01:04:40 -0500

    Update Shopping_system.java

--- Shopping_system/src/Shopping_system.java
+++ Shopping_system/src/Shopping_system.java
@@ -109,6 +109,9 @@ public class Shopping_system {
 							double money=in.nextDouble();
 							if(money<=total) {
 									System.out.println("金额不足！");
+							}else {
+								System.out.println("收款"+money+"元，应付款"+total+"元，找零"+(money-total)+"元");
+								System.out.println("欢迎下次再来！");;
 							}
 						}
 						
