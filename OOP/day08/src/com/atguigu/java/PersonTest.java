5f41405ae0e93f1c42831c02043792916990f01c
nortonlyr <nortonlyr@gmail.com>
2020-12-17 00:01:09 -0500

    UPDATED OOP

--- OOP/day08/src/com/atguigu/java/PersonTest.java
+++ OOP/day08/src/com/atguigu/java/PersonTest.java
@@ -6,6 +6,16 @@ public class PersonTest {
 	public static void main(String[] args) {
 		//创建Person类的对象
 		Person p1 = new Person();
+		
+		//调用对象的结构，属性，方法
+		//调用属性，“对象，属性”
+		p1.name = "Tom";
+		p1.isMale = true;
+		System.out.println(p1.name);
+		
+		//调用方法：“对象.方法”
+		p1.eat();
+		p1.sleep();
 	}
 }
 
