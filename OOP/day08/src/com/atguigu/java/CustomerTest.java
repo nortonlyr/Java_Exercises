aab61dec873d2aadf942af79d29d3e4ae1e963da
nortonlyr <nortonlyr@gmail.com>
2020-12-18 21:35:50 -0500

    OOP UPDATED

--- OOP/day08/src/com/atguigu/java/CustomerTest.java
+++ OOP/day08/src/com/atguigu/java/CustomerTest.java
@@ -1,5 +1,36 @@
 package com.atguigu.java;
 
 public class CustomerTest {
+	public static void main(String[] args) {
+		
+		Customer cust1 = new Customer();
+		
+		cust1.eat();
+	}
+}
 
+class Customer{
+	
+	//屬性
+	String name;
+	int age;
+	boolean isMale;
+	
+	//方法
+	public void eat(){
+		System.out.println("客戶吃飯");
+	}
+	
+	public void sleep(int hour){
+		System.out.println("休息了" + hour + "個小時");
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getNation(String nation){
+		String info = "我的國籍是： " + nation;
+		return info;
+	}
 }
