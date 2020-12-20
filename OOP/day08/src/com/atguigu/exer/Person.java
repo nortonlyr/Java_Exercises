f9a4eab0f801c16a216a8c34a25a74e6ae6fe261
nortonlyr <nortonlyr@gmail.com>
2020-12-19 22:34:15 -0500

    OOP UPDATED

--- OOP/day08/src/com/atguigu/exer/Person.java
+++ OOP/day08/src/com/atguigu/exer/Person.java
@@ -1,5 +1,25 @@
 package com.atguigu.exer;
 
 public class Person {
-
+	
+	String name;
+	int age;
+	/*
+	 * Sex = 1, 男， Sex = 0, 女
+	 */
+	
+	int sex;
+	
+	public void study(){
+		System.out.println("studying");
+	}
+	
+	public void showAge(){
+		System.out.println("age:" + age);
+	}
+	
+	public int addAge(int i){
+		age += i;
+		return age;
+	}
 }
--- OOP/day08/src/com/atguigu/exer/PersonTest.java
+++ OOP/day08/src/com/atguigu/exer/PersonTest.java
@@ -1,5 +1,24 @@
 package com.atguigu.exer;
 
 public class PersonTest {
-
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		
+		p1.name = "Tom";
+		p1.age = 18;
+		p1.sex = 1;
+		
+		p1.study();
+		
+		p1.showAge();
+		
+		int newAge = p1.addAge(2);
+		System.out.println(p1.name + "的新年齡為：" + newAge);
+		System.out.println(p1.age);
+		
+		Person p2 = new Person();
+		p2.showAge();
+		p2.addAge(10);
+		p2.showAge();
+	}
 }
